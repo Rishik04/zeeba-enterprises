0bb6382bf57765067432512c3405537955d96eab
Rishik <risikcharan04@gmail.com>
2025-12-11 19:43:54 +0530

    fixed the db name

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {dbName: "dev"});
+    const conn = await mongoose.connect(process.env.MONGO_URI, {dbName: process.env.DB_NAME});
 
     console.log(`✅ MongoDB connected: ${conn.connection.name}`);
   } catch (error) {
